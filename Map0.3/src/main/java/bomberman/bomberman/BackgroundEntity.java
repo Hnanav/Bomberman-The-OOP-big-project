1c621f9963d8a68691ea51809b54fc6f0ad0f30f
Rye1606Zz <49356827+thanhsonopera@users.noreply.github.com>
2022-10-07 15:13:38 +0700

    map

---
+++ Map0.3/src/main/java/bomberman/bomberman/BackgroundEntity.java
@@ -0,0 +1,11 @@
+package bomberman.bomberman;
+
+import java.awt.image.BufferedImage;
+
+public class BackgroundEntity extends Entity{
+    BackgroundEntity(int x, int y) {
+        super(x, y);
+    }
+
+    void getImage() {}
+}
---
+++ Map0.3/src/main/java/bomberman/bomberman/Beast.java
@@ -0,0 +1,265 @@
+package bomberman.bomberman;
+import javafx.scene.shape.Rectangle;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static bomberman.bomberman.CheckCollision.Collision;
+import static bomberman.bomberman.Game.*;
+
+public class Beast extends DynamicEntity {
+    private boolean isDied;
+
+    public Beast(int x, int y, int speed) throws IOException {
+        super(x, y, speed);
+        direction = setDirection();
+        getBeastImage();
+        isDied = false;
+    }
+    public String setDirection() {
+
+        int i = y / tileSize;
+        int j = x / tileSize;
+
+        if (j - 1 > 0)
+            if (map[i][j - 1] == '.') return "left";
+        if (j + 1 < 20)
+            if (map[i][j + 1] == '.') return "right";
+        if (i - 1 > 0)
+            if (map[i - 1][j] == '.') return "down";
+        if (i + 1 < 20)
+            if (map[i + 1][j] == '.') return "up";
+        return "right";
+    }
+    private void getBeastImage() {
+        try {
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_up_1.png")));
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_up_2.png")));
+            up3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_up_3.png")));
+            up4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_up_4.png")));
+            down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_down_1.png")));
+            down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_down_2.png")));
+            down3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_down_3.png")));
+            down4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_down_4.png")));
+            left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_left_1.png")));
+            left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_left_2.png")));
+            left3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_left_3.png")));
+            left4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_left_4.png")));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_right_1.png")));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_right_2.png")));
+            right3 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_right_3.png")));
+            right4 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Utils/Sprites/Beast_right_4.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public boolean isDied() {
+        for (BombExplosion i : BombExplosions) {
+            Rectangle rectangle = this.getPos();
+
+            Rectangle pos = i.getPos();
+            int left = i.getLeft_pole();
+            int right = i.getRight_pole();
+            int up = i.getUp_pole();
+            int down = i.getDown_pole();
+
+            Rectangle rec1_horizontal = new Rectangle(left, pos.getY(),
+                    right - left + tileSize, tileSize);
+            Rectangle rec1_vertical = new Rectangle(pos.getX(), up,
+                    tileSize, down - up + tileSize);
+
+            if (Collision(rectangle, rec1_horizontal) || Collision(rectangle, rec1_vertical)) {
+                isDied = true;
+                break;
+            }
+
+        }
+        return isDied;
+    }
+    public void update(int level) {
+        if (level <= 50) {
+            List<StaticEntity> walls = staticEntityList.getDesEntity();
+            List<StaticEntity> bricks = staticEntityList.getUndesEntity();
+            //BombExplosions;
+            boolean check = false;
+            String ndirection = direction;
+            String nowdirection = direction;
+            int cnt = 0;
+            while (!check) {
+                int luux = x, luuy = y;
+                if (ndirection == "up") cnt ++;
+                if (ndirection == "down") cnt ++;
+                if (ndirection == "left") cnt ++;
+                if (ndirection == "down") cnt ++;
+                boolean ft = true;
+                Rectangle now = new Rectangle();
+                switch (ndirection) {
+                    case "up":
+                        y = y - speed;
+                        break;
+                    case "down":
+                        y = y + speed;
+                        break;
+                    case "left":
+                        x = x - speed;
+                        break;
+                    case "right":
+                        x = x + speed;
+                        break;
+                }
+                now = new Rectangle(x, y, tileSize, tileSize);
+                if (x < 0 || x > screenWidth - tileSize) {
+                    ft = false; x = luux; y = luuy;
+                }
+                if (ft)
+                    if (y < 0 || y > screenHeight - tileSize) {
+                        ft = false; x = luux; y = luuy;
+                    }
+                if (ft)
+                    for (StaticEntity i : walls) {
+                        Rectangle next = i.getPos();
+                        if (Collision(now, next)) { ft = false; x = luux; y = luuy; break;}
+                    }
+                if (ft)
+                    for (StaticEntity i : bricks) {
+                        Rectangle next = i.getPos();
+                        if (Collision(now, next)) {ft = false; x = luux; y = luuy; break;}
+                    }
+                if (ft)
+                    for (BombExplosion i : BombExplosions) {
+                        Rectangle next1 = new Rectangle(i.getLeft_pole(), i.getRight_pole(), tileSize, tileSize);
+                        Rectangle next2 = new Rectangle(i.getUp_pole(), i.getDown_pole(), tileSize, tileSize);
+                        if (Collision(now, next1)) {ft = false; break;}
+                        if (Collision(now, next2)) {ft = false; break;}
+                    }
+                if (ft) {
+                    List<Bomb> Bombs = bombList.getBombs();
+                    for (Bomb i : Bombs) {
+                        Rectangle next = i.getPos();
+                        if (Collision(now, next)) {ft = false; x = luux; y = luuy; break;}
+                    }
+                }
+                if (ft) {
+                    for (Snake i : snaker) {
+                            Rectangle next = i.getPos();
+                            if (Collision(now, next)) {ft = false; x = luux; y = luuy; break;}
+                        }
+                }
+                if (ft) {
+                    for (Beast i : beasts)
+                        if (this != i) {
+                        Rectangle next = i.getPos();
+                        if (Collision(now, next)) {ft = false; x = luux; y = luuy; break;}
+                    }
+                }
+                if (ft) {
+                    check = true;
+                    direction = ndirection;
+                    if (direction == nowdirection) {
+                        spriteCounter++;
+                        if (spriteCounter > 3) {
+                            spriteNum = (spriteNum % 4) + 1;
+                            spriteCounter = 0;
+                        }
+                    }
+                    else {
+                        spriteNum = 1;
+                        spriteCounter = 0;
+                    }
+                }
+                if (!ft) {
+                    if (cnt > 4) {
+                        x = luux; y = luuy; check = true;
+                        break;
+                    }
+                    int pos = ThreadLocalRandom.current().nextInt(1, 5);
+                    switch (pos) {
+                        case 1:
+                            ndirection = "up";
+                            //System.out.println("loz99999999");
+                            break;
+                        case 2:
+                            ndirection = "down";
+                            //System.out.println("loz99999");
+                            break;
+                        case 3:
+                            ndirection = "left";
+                            //System.out.println("loz999");
+                            break;
+                        case 4:
+                            ndirection = "right";
+                            //System.out.println("loz99");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+    public void render(Graphics2D g2) {
+        image = null;
+        switch (direction) {
+            case "up":
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                else if (spriteNum == 2) {
+                    image = up2;
+                }
+                else if (spriteNum == 3) {
+                    image = up3;
+                }
+                else if (spriteNum == 4) {
+                    image = up4;
+                }
+                break;
+            case "down":
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                else if (spriteNum == 2) {
+                    image = down2;
+                }
+                else if (spriteNum == 3) {
+                    image = down3;
+                }
+                else if (spriteNum == 4) {
+                    image = down4;
+                }
+                break;
+            case "left":
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                else if (spriteNum == 2) {
+                    image = left2;
+                }
+                else if (spriteNum == 3) {
+                    image = left3;
+                }
+                else if (spriteNum == 4) {
+                    image = left4;
+                }
+                break;
+            case "right":
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                else if (spriteNum == 2) {
+                    image = right2;
+                }
+                else if (spriteNum == 3) {
+                    image = right3;
+                }
+                else if (spriteNum == 4) {
+                    image = right4;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, tileSize, tileSize , null);
+    }
+}
