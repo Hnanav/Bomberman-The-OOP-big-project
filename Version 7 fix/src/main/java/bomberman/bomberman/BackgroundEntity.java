32b97bc602cae0e2cefc186e2b72db5a16200b8c
X <quangh2019@gmail.com>
2022-10-12 23:37:18 +0700

    Garbage Collection

--- Version 7 fix/src/main/java/bomberman/bomberman/BackgroundEntity.java
+++
@@ -1,9 +0,0 @@
-package bomberman.bomberman;
-
-public class BackgroundEntity extends Entity{
-    BackgroundEntity(int x, int y) {
-        super(x, y);
-    }
-
-    void getImage() {}
-}
--- Version 7.4.1/src/main/java/bomberman/bomberman/Main.java
+++
@@ -1,7 +0,0 @@
-package bomberman.bomberman;
-
-public class Main {
-    public static void main(String[] args) {
-        LaunchMenu launchMenu = new LaunchMenu();
-    }
-}
